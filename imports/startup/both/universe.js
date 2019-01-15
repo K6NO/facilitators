24801140698aeff6ef13567f8f4c774adc4a094b
K6NO <tamas.kenessey@gmail.com>
2019-01-15 10:14:03 +0100

    Create translation JSONs for all languages, add localization imports to universe.js

--- imports/startup/both/universe.js
+++ imports/startup/both/universe.js
@@ -1,3 +1,5 @@
-// import localisation JSON files here
 import '../../localizations/en.i18n.json';
+import '../../localizations/es.i18n.json';
 import '../../localizations/hu.i18n.json';
+import '../../localizations/ro.i18n.json';
+import '../../localizations/sk.i18n.json';
