98ec7db13d266a2074dab8f2af650a212d58ab67
K6NO <tamas.kenessey@gmail.com>
2019-01-15 21:17:20 +0100

    Add sketch file temporarilyil

---
+++ imports/modules/get-colors.js
@@ -0,0 +1,11 @@
+export const getColorByCategory = (category) => {
+    return {
+        'Ecofeminism' : "#0e8ed5",
+        'Deep ecology' : "#57c214",
+        'Environmental footprint' : "#a17278",
+        'Land art' : '#f7820e',
+        'Food sovereignity' : '#d51f1b',
+        'Community building' : '#9b6e33',
+    }[category];
+} 
+
