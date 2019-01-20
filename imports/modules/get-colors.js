be2f100f28832a84814ae823531bd54526f9b969
K6NO <tamas.kenessey@gmail.com>
2019-01-20 21:52:59 +0630

    getColors function uses shorthand category definitions

--- imports/modules/get-colors.js
+++ imports/modules/get-colors.js
@@ -1,11 +1,11 @@
 export const getColorByCategory = (category) => {
     return {
-        'Ecofeminism' : "#0e8ed5",
-        'Deep ecology' : "#57c214",
-        'Environmental footprint' : "#a17278",
-        'Land art' : '#f7820e',
-        'Food sovereignity' : '#d51f1b',
-        'Community building' : '#9b6e33',
+        'ecofem' : "#0e8ed5",
+        'deepeco' : "#57c214",
+        'footprint' : "#a17278",
+        'landart' : '#f7820e',
+        'food' : '#d51f1b',
+        'community' : '#9b6e33',
     }[category];
 } 
 
