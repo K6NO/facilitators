e39eed1afe8444f23ba58cc6d5d34642c8b8e394
K6NO <tamas.kenessey@gmail.com>
2019-01-20 13:30:33 +0630

    Schema defines age, participants, time in strings  without translations. Localization file holds the translations of these categories

--- imports/api/Activities/Activities.js
+++ imports/api/Activities/Activities.js
@@ -126,111 +126,108 @@ Activities.schema = new SimpleSchema({
         max: 1200,
         label: "Activity description. Max. 1200 chars."
     },
-    category: Object,
-    'category.en': {
+    category: {
         type: String,
         max: 40,
         label: "Activity category. Max. 40 chars.",
-        allowedValues: ['Ecological footprint', "Deep ecology", "Ecofeminism", "Land art", "Food sovereignity", "Community building"],
+        allowedValues: ["footprint", "deepeco", "ecofem", "landart", "food", "community"],
     },
-    'category.es': {
+    age: {
         type: String,
-        max: 40,
-        label: "Activity category. Max. 40 chars.",        
+        max: 10,
+        label: "Activity age. Max. 10 chars.",
+        allowedValues: ["+5", "+8", "+12", "+15", "+18"],
     },
-    'category.hu': {
+    time: {
         type: String,
-        max: 40,
-        label: "Activity category. Max. 40 chars.",
+        max: 10,
+        label: "Activity time. Max. 10 chars.",
+        allowedValues: ["10-15", "15-20", "20-25", "25-30", "30-45", "45-60", "60-90", "90-120", "+120"],
     },
-    'category.ro': {
+    participants: {
         type: String,
-        max: 40,
-        label: "Activity category. Max. 40 chars",
+        max: 10,
+        label: "Activity time. Max. 10 chars.",
+        allowedValues: ["+2", "+3", "+5", "+8", "+10"],
     },
-    'category.sk': {
-        type: String,
-        max: 40,
-        label: "Activity category. Max. 40 chars",
-    },
-    age: Object,
-    'age.en': {
-        type: String,
-        max: 20,
-        label: "Activity age group. Max. 20 chars."
-    },
-    'age.es': {
-        type: String,
-        max: 20,
-        label: "Activity age group. Max. 20 chars."
-        },
-    'age.hu': {
-        type: String,
-        max: 20,
-        label: "Activity age group. Max. 20 chars."
-    },
-    'age.ro': {
-        type: String,
-        max: 20,
-        label: "Activity age group. Max. 20 chars."
-    },
-    'age.sk': {
-        type: String,
-        max: 20,
-        label: "Activity age group. Max. 20 chars."
-    },
-    time: Object,
-    'time.en': {
-        type: String,
-        max: 20,
-        label: "Activity time. Max. 20 chars."
-    },
-    'time.es': {
-        type: String,
-        max: 20,
-        label: "Activity time. Max. 20 chars."
-    },
-    'time.hu': {
-        type: String,
-        max: 20,
-        label: "Activity time. Max. 20 chars."
-    },
-    'time.ro': {
-        type: String,
-        max: 20,
-        label: "Activity time. Max. 20 chars."
-    },
-    'time.sk': {
-        type: String,
-        max: 20,
-        label: "Activity time. Max. 20 chars."
-    },
-    participants: Object,
-    'participants.en': {
-        type: String,
-        max: 20,
-        label: "Activity nr of participants. Max. 20 chars."
-    },
-    'participants.es': {
-        type: String,
-        max: 20,
-        label: "Activity nr of participants. Max. 20 chars."
-    },
-    'participants.hu': {
-        type: String,
-        max: 20,
-        label: "Activity nr of participants. Max. 20 chars."
-    },
-    'participants.ro': {
-        type: String,
-        max: 20,
-        label: "Activity nr of participants. Max. 20 chars."
-    },
-    'participants.sk': {
-        type: String,
-        max: 20,
-        label: "Activity nr of participants. Max. 20 chars."
-    },
+    // age: Object,
+    // 'age.en': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity age group. Max. 20 chars."
+    // },
+    // 'age.es': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity age group. Max. 20 chars."
+    //     },
+    // 'age.hu': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity age group. Max. 20 chars."
+    // },
+    // 'age.ro': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity age group. Max. 20 chars."
+    // },
+    // 'age.sk': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity age group. Max. 20 chars."
+    // },
+    // time: Object,
+    // 'time.en': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity time. Max. 20 chars."
+    // },
+    // 'time.es': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity time. Max. 20 chars."
+    // },
+    // 'time.hu': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity time. Max. 20 chars."
+    // },
+    // 'time.ro': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity time. Max. 20 chars."
+    // },
+    // 'time.sk': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity time. Max. 20 chars."
+    // },
+    // participants: Object,
+    // 'participants.en': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity nr of participants. Max. 20 chars."
+    // },
+    // 'participants.es': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity nr of participants. Max. 20 chars."
+    // },
+    // 'participants.hu': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity nr of participants. Max. 20 chars."
+    // },
+    // 'participants.ro': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity nr of participants. Max. 20 chars."
+    // },
+    // 'participants.sk': {
+    //     type: String,
+    //     max: 20,
+    //     label: "Activity nr of participants. Max. 20 chars."
+    // },
     preparations: Object,
     'preparations.en': {
         type: String,
