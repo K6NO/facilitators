c7b3570ca860c51b7bd609124f41b4313ee3f736
K6NO <tamas.kenessey@gmail.com>
2019-01-27 22:28:16 +0630

    Activities publication with filtered result according to search criteria  added

--- imports/api/Activities/server/publications.js
+++ imports/api/Activities/server/publications.js
@@ -33,31 +33,46 @@ Meteor.publish('activities.all', () => {
     });
 });
 
-// Meteor.publish('activities.allByLang', (locale) => {
-//     const userId = Meteor.userId();
-//     let languages = ['en', 'es', 'hu', 'ro', 'sk'];
-//     if(userId) {
-//         if(Roles.userIsInRole(userId, ['admin'])) {
-//             const activities = Activities.find();
-//             return getActivitiesByLang(locale, languages, activities);
-//         }
-//         if(Roles.userIsInRole(userId, ['editor'])) {
-//             const activities = Activities.find({
-//                 $or: [
-//                     { owner : userId },
-//                     { public: true }
-//                 ]
-//             });
-//             return getActivitiesByLang(locale, languages, activities);
-//         }
-//         const activities = Activities.find({
-//             $or: [{ 
-//                 public: true
-//             }]
-//         });
-//         return getActivitiesByLang(locale, languages, activities);
-//     }
-// });
+Meteor.publish('activities.allFilter', (filterObject) => {
+    check(filterObject, Object);
+    /**
+     * Querying the db with multiple, non-exclusive criteria.
+     * 1. Examine the filterObject, check for existing key-value pairs
+     * 2. Values of the filterObject must be in an array
+     * 3. Keys of the filterObject must match allowedFields in Activities schema
+     * 4. Find documents matching any of these values in the corresponding fields.
+     *    Use the $and operator, in combination with $or if needed.  
+     * 5. Make sure that only public or own content is returned for non admins
+     * 
+     * Inspired by https://stackoverflow.com/questions/49628432/how-to-convert-an-object-to-array-of-objects#
+     */
+
+    const mongoFilterArray = Object.entries(filterObject).map(([key, values]) => (
+        { key : {$in : values} }
+    ))
+    
+    const userId = Meteor.userId();
+    if(userId) {
+        if(Roles.userIsInRole(userId, ['admin'])) {
+            return Activities.find({
+                $and: mongoFilterArray
+            });
+        }
+        return Activities.find({
+            $or:[
+                { public: true },
+                { owner: userId }                
+            ],
+            $and: mongoFilterArray
+        });
+    }
+    // TODO - see if this works. If not, then use push
+    // mongoFilterArray.push({public: true}) 
+    return Activities.find({
+        public: true,
+        $and: mongoFilterArray
+    });
+});
 
 Meteor.publish('activities.view', (activityId) => {
     check(activityId, String);
@@ -82,77 +97,3 @@ Meteor.publish('activities.view', (activityId) => {
     });
     
 });
-
-// Meteor.publish('activities.viewByLang', (activityId, locale) => {
-//     check(activityId, String);
-//     check(locale, String);
-//     const userId = Meteor.userId();
-//     let languages = ['en', 'es', 'hu', 'ro', 'sk'];
-
-//     if(userId) {
-//         if(Roles.userIsInRole(userId, ['admin'])) {
-//             const activity = Activities.find({
-//                 _id : activityId,
-//             });
-//             return getActivitiesByLang(locale, languages, activity);
-//         }
-//         if(Roles.userIsInRole(userId, ['editor'])) {
-//             const activity = Activities.find({
-//                 _id: activityId,
-//                 $or: [
-//                     { owner : userId },
-//                     { public: true }
-//                 ]
-//             });
-//             return getActivitiesByLang(locale, languages, activity);
-//         }
-//         const activity = Activities.find({
-//             _id: activityId,
-//             $or: [{ 
-//                 public: true
-//             }]
-//         });
-//         return getActivitiesByLang(locale, languages, activity);
-//     }
-// });
-
-// Meteor.publish('activities.criteriaByLang', (field, criteria, tags, locale) => {
-//     check(field, String);
-//     check(criteria, String);
-//     check(tags, Array);
-//     const userId = Meteor.userId();
-//     let languages = ['en', 'es', 'hu', 'ro', 'sk'];
-
-//     if(userId) {
-//         if(Roles.userIsInRole(userId, ['admin'])) {
-//             const activities = Activities.find({
-//                 $or: [
-//                     { [field] : criteria },
-//                     { tags : {$regex: tags} }
-//                 ]
-//             });
-//             return getActivitiesByLang(locale, languages, activities);
-//         }
-//         if(Roles.userIsInRole(userId, ['editor'])) {
-//             const activities = Activities.find({
-//                 $or: [
-//                     { [field] : criteria },
-//                     { tags : {$regex: tags} },
-//                 ],
-//                 $or: [
-//                     { owner : userId },
-//                     { public: true }
-//                 ]
-//             });
-//             return getActivitiesByLang(locale, languages, activities);
-//         }
-//         const activities = Activities.find({
-//             $or: [{ 
-//                 [field] : criteria,
-//                 tags : {$regex: tags}
-//             }],
-//             public: true
-//         });
-//         return getActivitiesByLang(locale, languages, activities);
-//     }
-// });
