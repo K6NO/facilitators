e9634ddfae519012622718d4fd2507e2e0db7355
K6NO <tamas.kenessey@gmail.com>
2019-01-28 05:47:17 +0630

    Add language and category selectors to public navigation

--- imports/ui/components/PublicNavigation/PublicNavigation.js
+++ imports/ui/components/PublicNavigation/PublicNavigation.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Nav, NavItem  } from 'reactstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-
+import LanguageSelector from '../LanguageSelector/LanguageSelector';
 import LoginModalController from '../../components/LoginModal/LoginModalController';
 import './PublicNavigation.scss';
 
 const PublicNavigation = props => (
   <div className="PublicNavigation">
     <Nav navbar>
-      {props.viewportIsMobile && 
       <NavItem 
-        id="imageLi"
         onClick={props.navbarCallback}>
-        <img className="menuLogo" src="/img/ui/NewsGamer_logo_white.png" alt="Newsgamer logo"/>
+        <LanguageSelector {...props} /> 
       </NavItem>
-      }
+      <NavItem 
+        onClick={props.navbarCallback}>
+        Categories
+      </NavItem>
+      <NavItem 
+        onClick={props.navbarCallback}>
+        About
+      </NavItem>
       <NavItem
         onClick={props.navbarCallback}>
         <LoginModalController            
-          className={"menuModal"} 
           {...props}/>
       </NavItem>
     </Nav>
