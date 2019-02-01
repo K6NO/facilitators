91fbb1629535524cdd57b37ed1b8a2c1b55e41db
K6NO <tamas.kenessey@gmail.com>
2019-02-01 20:50:17 +0630

    Add function to update locale in LanguageSelector

--- imports/ui/components/LanguageSelector/LanguageSelector.js
+++ imports/ui/components/LanguageSelector/LanguageSelector.js
@@ -2,24 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 import i18n from 'meteor/universe:i18n';
+import setLocale from '../../../modules/set-locale';
 import getLocale from '../../../modules/get-locale';
 import './LanguageSelector.scss';
 
 class LanguageSelector extends React.Component {
 
+    updateLocale = (localeObject) => {    
+        setLocale(localeObject.value);
+      }
+
     
     render () {
         const languageArray = [
-            { value: 'en', label: i18n.__('searchbox.en')},
+            { value: 'en-US', label: i18n.__('searchbox.en-US')},
             { value: 'es', label: i18n.__('searchbox.es')},
             { value: 'hu', label: i18n.__('searchbox.hu')},
             { value: 'ro', label: i18n.__('searchbox.ro')},
             { value: 'sk', label: i18n.__('searchbox.sk')},
         ];
         const locale = getLocale();
-        const label = languageArray.find(e => e.value === locale).label;        
-        // const defaultText = languageArray[languageArray.indexOf(this.state.locale)].value;
-        // console.log(defaultText);
+        
+        const label = languageArray.filter(e => e.value === locale)[0].label;
+        
         
         return (
             <Select 
@@ -30,7 +35,7 @@ class LanguageSelector extends React.Component {
                 options={languageArray}
                 defaultValue={label}
                 // value={this.state.locale}
-                onChange={this.props.updateLocaleCallback}
+                onChange={this.updateLocale}
                 aria-label="Select Language"
                 name="LanguageSelector"
                 />
@@ -39,7 +44,6 @@ class LanguageSelector extends React.Component {
 }
   
 LanguageSelector.propTypes = {
-    updateLocaleCallback : PropTypes.func.isRequired,
 };
   
 export default LanguageSelector;
