e085c3c88fa62721575b2cc852588639b295604d
K6NO <tamas.kenessey@gmail.com>
2019-02-06 08:27:54 +0630

    Add jsx and scss elements to activity header

--- imports/ui/components/ActivityComponent/ActivityComponentHeader.js
+++ imports/ui/components/ActivityComponent/ActivityComponentHeader.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { getColorByCategory } from '../../../modules/get-colors';
 import Icon from '../Icon/Icon';
-import { Row, Col, Table, FormGroup } from 'reactstrap';
-
-import './ActivityComponent.scss';
+import { Row, Col, Badge } from 'reactstrap';
+import './ActivityComponentHeader.scss';
+import getLocale from '../../../modules/get-locale';
+import { getCategoryName } from '../../../modules/get-category-name';
 
 class ActivityComponentHeader extends React.Component {
     constructor(props){
@@ -15,17 +16,44 @@ class ActivityComponentHeader extends React.Component {
         return {
             background : color
         }
-    } 
+    }
+
+    renderTags = ({activity, locale}) => {
+        return activity.tags.map((tagIndex) =>   
+            <Badge 
+                color="light" 
+                pill
+                key={tagIndex}
+                >
+                {i18n.__(`tags.${tagIndex}`)}
+            </Badge>
+        );
+
+    }
     render() {
-        // const { activity } = this.props;
-        // const category = activity.category;
-        const category = 'deepeco';
+        const { activity, locale } = this.props;
+        const category = activity.category;
         const color = getColorByCategory(category);
+        const categoryName = getCategoryName(category);
         return (
-            <div className="ActivityComponentHeader"
-                style={this.setBackground(color)}>
-                
-                <Icon icon={'back'} size={'lg'}/>Back to search results 
+            <div className="ActivityComponentHeader">
+                <Row>
+                    <Col sm="6" style={this.setBackground(color)}>
+                        <h2 className="activityTitle">{activity.title[`title.${locale}`]}</h2>
+                        <a className="activityCategory">{categoryName}</a>
+                        <br/>
+                        {/* for each number in the tags array find the corresponding tag in the i18 file and render */}
+                        {this.renderTags({activity, locale})}
+                        
+                        {/* <Badge color="light" pill>Light</Badge>
+                        <Badge color="light" pill>Light</Badge>
+                        <Badge color="light" pill>Light</Badge> */}
+                    </Col>
+                    <Col sm="6">
+                    
+                        <Icon icon={'back'} size={'lg'}/>Back to search results 
+                    </Col>
+                </Row>
             </div>
         )
     }
@@ -37,6 +65,7 @@ ActivityComponentHeader.defaultProps = {
   
 ActivityComponentHeader.propTypes = {
     activity: PropTypes.object.isRequired,
+    locale: PropTypes.string.isRequired,
 };
 
 
