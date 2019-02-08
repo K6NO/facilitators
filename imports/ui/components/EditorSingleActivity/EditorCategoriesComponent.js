0bcb0bc041c854d594cea66ee7bd9593008f386a
K6NO <tamas.kenessey@gmail.com>
2019-02-08 19:18:24 +0630

    Create category selector component

---
+++ imports/ui/components/EditorSingleActivity/EditorCategoriesComponent.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import Select from 'react-select';
+import Icon from '../Icon/Icon';
+import { getCategoryName, getCategoryArray } from '../../../modules/get-category-name';
+
+class EditorCategoriesComponent extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+          category : getCategoryName(this.props.activity.category),
+      }
+    }
+
+    saveCategory = (selection) => {
+        this.setState({category: selection.value})
+        const activityId = this.props.activity._id;
+        Meteor.call('activities.updateAttributes', 
+        activityId, 'category', selection.value,
+        (error) => {
+            if(error) {
+                Bert.alert(error.reason, 'danger');
+            } else { 
+                Bert.alert('Saved changes', 'success');
+            }
+        });
+    }
+
+    render() {
+        const categoryArray = getCategoryArray();
+        return (
+            <div className="EditorCategoriesComponent">
+                 <Select 
+                    className="basic-single CategorySelector"
+                    classNamePrefix="category-edit"
+                    isSearchable={false}
+                    isClearable={false}
+                    options={categoryArray}
+                    name="categoryEditSelect"
+                    value={this.state.category}
+                    onChange={(selection) => this.saveCategory(selection)}
+                    aria-label="Edit Category"
+                />
+            </div>
+        )
+    }
+}
+
+EditorCategoriesComponent.defaultProps = {
+};
+  
+EditorActivityHeader.propTypes = {
+    activity: PropTypes.object.isRequired,
+};
+
+
+export default EditorCategoriesComponent;
+
