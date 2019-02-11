50be9c4a58fd8f8129dcf3dfce9766e46ee79c3c
K6NO <tamas.kenessey@gmail.com>
2019-02-11 07:58:33 +0630

    Add json mock new Activity and import in EditorNewActivityButton

--- imports/ui/components/EditorNewActivityButton/EditorNewActivityButton.js
+++ imports/ui/components/EditorNewActivityButton/EditorNewActivityButton.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './EditorNewActivityButton.scss';
+import newActivity from './mockNewActivity.json';
 
 class EditorNewActivityButton extends React.Component{
   constructor(props){
     super(props);
   }
  
+  createActivity = () => {
+    
+  }
   render (){
     return ( 
       <div className="EditorPage">
