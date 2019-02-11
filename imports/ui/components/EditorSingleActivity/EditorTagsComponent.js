2b53f3b623c2e2eaa451c2310e9d12f4ec0c6d81
K6NO <tamas.kenessey@gmail.com>
2019-02-11 07:42:45 +0630

    Create EditorTagsComponent for editor header

---
+++ imports/ui/components/EditorSingleActivity/EditorTagsComponent.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import Select from 'react-select';
+import i18n from 'meteor/universe:i18n';
+
+class EditorTagsComponent extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+          editing: false
+      }
+    }
+
+    getTags = () => {
+        const tagsObject = i18n.__('tags');
+        const tagsArray = Object.values(tagsObject);
+        return tagsArray;
+    }
+
+    addTag = () => {
+        this.setState({editing: true});
+        // create a dropdown with tag names
+        // selection adds tag
+    }
+    updateTitle = (e) => {
+        this.setState({title: e.value})
+    }
+    saveTag = (selection) => {
+        const { activity } = this.props;
+        const oldTags = activity.tags;
+        // find the index of the newly selected tag in the list of tags 
+        const newTagIndex = getTags().indexOf(selection.value);
+        // push the new selection's index in the array of tags
+        oldTags.push(newTagIndex);
+        // save
+        Meteor.call('activities.updateAttributes', activity._id, 'tags', oldTags, 
+        (error) => {
+            if(error) {
+                Bert.alert(error.reason, 'danger');
+            } else { 
+                Bert.alert('Saved changes', 'success');
+            }
+        });
+    }
+
+    renderTags = ({activity, color}) => {
+        return activity.tags.map((tagIndex) =>   
+            <Badge 
+                color="light" 
+                pill
+                key={tagIndex}
+                className="tagPills"
+                style={{color: color}}
+                >
+                {i18n.__(`tags.${tagIndex}`)}
+            </Badge>
+        );
+    }
+
+    render() {
+        return (
+            <div className="EditorTagsComponent">
+                {this.renderTags({activity, color})}
+                {!editing 
+                ? <button>Add Tag</button>
+                : <Select 
+                    className="basic-single CategorySelector"
+                    classNamePrefix="tags-edit"
+                    isSearchable={false}
+                    isClearable={false}
+                    options={this.getTags()}
+                    name="categoryEditSelect"
+                    onChange={(selection) => this.saveTag(selection)}
+                    aria-label="Edit Category"
+                />  }
+            </div>
+        )
+    }
+}
+
+EditorTagsComponent.defaultProps = {
+};
+  
+EditorActivityHeader.propTypes = {
+    activity: PropTypes.object.isRequired,
+    language: PropTypes.string.isRequired,
+};
+
+
+export default EditorTagsComponent;
+
